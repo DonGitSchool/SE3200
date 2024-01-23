ebd0f544342351f07f2b21ee60633227c109d7ab
DonGitSchool <d00417722@utahtech.edu>
2024-01-23 09:01:54 -0700

    Gogo

--- helloJS/app.js
+++ helloJS/app.js
@@ -1,11 +1,20 @@
 console.log("connected");
 
 function hello(){
-    document.getElementById("demo").innerHTML = "Hello JavaScript";
+    var button = document.getElementById("HelloButton");
     var el = document.getElementById("name");
-    console.log("name:", el);
-    el.innerHTML = "Hello JavaScript";
-    el.style.color = "green";
-    var image1 = document.getElementById("image1");
+    var oldinner= document.getElementById("")
+    if (button.innerHTML === "Say Hello") {
+        console.log("name:", el);
+        el.innerHTML = "Hello JavaScript";
+        el.style.color = "green";
+        document.getElementById("image1").src="https://i.gifer.com/VoBm.gif";
+        button.innerHTML = "Say Goodbye";
+    } else if (button.innerHTML === "Say Goodbye") {
+        // Reset to original values
+        el.innerHTML = "Goodbye JavaScript";
+        el.style.color = "";
+        document.getElementById("image1").src = "https://i.pinimg.com/originals/a3/55/59/a355591fa63bab38bf68b43118132543.gif";
+        button.innerHTML = "Say Hello";
+    }
 }
-
