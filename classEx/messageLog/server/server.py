52d94ae1d0584c99551e2cf06f9ad0eb0e1b26cf
DonGitSchool <d00417722@utahtech.edu>
2024-02-29 10:10:27 -0700

    Fin Class

--- classEx/messageLog/server/server.py
+++ classEx/messageLog/server/server.py
@@ -1,20 +1,25 @@
 from flask import Flask, request, jsonify
-from dummydb import DummyDB
+from rollercoasters import RolllerCoastersDB
 
 app = Flask(__name__)
-db = DummyDB("dummydb.db")
-print(db.readAllRecords())
+db = RolllerCoastersDB("rollercoasters.db")
+print(db.getRollerCoasters())
 
 @app.route("/rollercoasters", methods=["GET"])
 def retreive_coasters_collection():
+    db = RolllerCoastersDB("rollercoasters.db")
     #load from db
-    rollercoasters= db.readAllRecords()
+    rollercoasters= db.getRollerCoasters()
     return jsonify(rollercoasters), 200, {"Access-Control-Allow-Origin": "*"}
 
 @app.route("/rollercoasters", methods=["POST"])
 def create_in_coasters_collection():
+    db = RolllerCoastersDB("rollercoasters.db")
     print("The request data is: ", request.form)
-    db.saveRecord(request.form["name"])
+    name = request.form["name"]
+    review = request.form["review"]
+    rating = request.form["rating"]
+    db.createRollerCoasters(name,review,rating)
     return "created",201, {"Access-Control-Allow-Origin":"*"}
 
 def run():
