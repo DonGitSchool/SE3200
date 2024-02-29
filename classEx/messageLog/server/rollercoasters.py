62ede5717870f994eeb55e53afde2cae0ab35b5e
DonGitSchool <d00417722@utahtech.edu>
2024-02-29 09:52:55 -0700

    Continue

--- classEx/messageLog/server/rollercoasters.py
+++ classEx/messageLog/server/rollercoasters.py
@@ -1,34 +1,54 @@
 import sqlite3
 
-#connect to the DB file
-connection = sqlite3.connect("rollercoasters.db")
+class RolllerCoastersDB:
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(filename)
+        self.cursor = self.connection.cursor()
 
+    def getRollerCoasters(self):
+        self.cursor.execute("SELECT * FROM rollercoasters")
+        rollercoasters = self.cursor.fetchall()
+        return rollercoasters
+
+    def createRollerCoasters(self, name, review, rating):
+        self.cursor.execute("INSERT INTO rollercoasters (name,review,rating) VALUES (?,?,?)",(name,review,rating))
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+        
+######################################################
+#connect to the DB file
+#connection = sqlite3.connect("rollercoasters.db")
 
 #use the connecction instance to perform DB operations
 #create a cursor instance for our connection
-cursor = connection.cursor()
+#cursor = connection.cursor()
 
 #ANYTIME BEFORE FETCH
 
-cursor.execute("SELECT * FROM rollercoasters")
+#cursor.execute("SELECT * FROM rollercoasters")
 
 #fetchall - always returns an array of tuples, but could be empty
-rollercoasters = cursor.fetchall()
-print("before adding",rollercoasters)
+#rollercoasters = cursor.fetchall()
+#print("before adding",rollercoasters)
 
-cursor.execute("SELECT * FROM rollercoasters")
+#cursor.execute("SELECT * FROM rollercoasters")
 #fetchone - always returns the first tuple or None
-rollercoasters = cursor.fetchone()
-print("rollercoasters",rollercoasters)
+#rollercoasters = cursor.fetchone()
+#print("rollercoasters",rollercoasters)
 
 #add new rollercoaster review
-cursor.execute("INSERT INTO rollercoasters (name,review,rating) VALUES ('X2','I was not expecing that',10)")
+#cursor.execute("INSERT INTO rollercoasters (name,review,rating) VALUES ('X2','I was not expecing that',10)")
 
+#commit the change
+#connection.commit()
 
-cursor.execute("SELECT * FROM rollercoasters")
+#cursor.execute("SELECT * FROM rollercoasters")
 #fetchall - always returns an array of tuples, but could be empty
-rollercoasters = cursor.fetchall()
-print("after adding",rollercoasters)
+#rollercoasters = cursor.fetchall()
+#print("after adding",rollercoasters)
+
 
 #close connection
-connection.close()
+#connection.close()
